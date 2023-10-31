cd391c77d61f8e2b2499a0dc75cf984ed305d75d
Hendrik Langer <hendrik+dev@xd0.de>
2023-10-31 11:33:00 +0100

    test offload_folder

--- streaming_llm/utils.py
+++ streaming_llm/utils.py
@@ -58,10 +58,10 @@ def load(model_name_or_path):
     model = AutoModelForCausalLM.from_pretrained(
         model_name_or_path,
         device_map="auto",
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
         offload_folder="offload",
         offload_state_dict=True,
+        torch_dtype=torch.float16,
+        trust_remote_code=True,
     )
     if tokenizer.pad_token_id is None:
         if tokenizer.eos_token_id is not None:
